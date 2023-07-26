6d04f55e03eddbab06eb9483b2e7fa7959d480ee
Karlito14 <leiroz26@hotmail.com>
2023-07-26 19:38:35 +0200

    création du composant container NoteList

---
+++ src/container/NoteList/NoteList.jsx
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux';
+import style from './style.module.css';
+import { TextCard } from 'components/TextCard/TextCard';
+import { useNavigate } from 'react-router-dom';
+
+export const NoteList = () => {
+    const noteList = useSelector(store => store.NOTE.noteList);
+    const navigate = useNavigate();
+    
+    return (
+        <div className={`row ${style.cards_container}`}>
+            {noteList.map(note =>{
+                return (
+                    <div className={style.card_container} key={note.title + note.id}>
+                        <TextCard
+                            title={note.title}
+                            date={note.created_at}
+                            content={note.content}
+                            onClick={() => {navigate(`/note/${note.id}`)}}
+                            onClickTrash={() => {}}
+                        />
+                    </div>
+                )
+                
+            })}
+        </div>
+    )
+};
--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,18 +1,9 @@
 import { TextCard } from 'components/TextCard/TextCard';
 import style from './style.module.css';
+import { NoteList } from 'container/NoteList/NoteList';
 
 export const NoteBrowse = () => {
     return (
-        <TextCard
-            title='test'
-            date='01/01/2023'  
-            content='Super description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description textSuper description text'
-            onClickTrash={() => {
-                alert('click trash')
-            }}
-            onClick={() => {
-                alert('click note')
-            }}
-        />
+            <NoteList />  
     )
 };  
